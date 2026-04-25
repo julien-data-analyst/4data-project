492619e395f0f1fe1d61f94e587ee95303c1df22
julien-data-analyst <julienrenoult76460@gmail.com>
2026-04-25 19:54:21 +0200

    INFO : modifié transformations dbt et déplacement du schemas.yml pour les sources

--- dbt_informatics_companies/models/marts/codes_naf_informatics.sql
+++ dbt_informatics_companies/models/marts/codes_naf_informatics.sql
@@ -5,5 +5,5 @@ config(
 )
 }}
 SELECT subclasses_codes, subclasses_title, classe_title, classes_codes, section_title
-FROM {{ source('codes_nafs', 'code_naf') }}
+FROM {{ source('codes_nafs_companies', 'code_naf') }}
 WHERE classes_codes IN ('62', '63') /* code naf informatique */
